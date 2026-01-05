cd4007ae1f512c4fce2778a510e01ba20c380f71
NovaCrypt <novacrypt0512@pm.me>
2026-01-05 12:35:34 +0000

    Remove custom package configurations

--- config/dmenu/scripts/tax-year.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-case "$(echo -e "2025\n2024\n2023" | dmenu -i)" in
-    2025) libreoffice ~/Documents/01-Work/01-Finances/2025-TaxReport.ods ;;
-    2024) libreoffice ~/Documents/01-Work/01-Finances/2024-TaxReport.xlsx ;;
-    2023) libreoffice ~/Documents/01-Work/01-Finances/2023-TaxReport.xlsx ;;
-esac
