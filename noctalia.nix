06e209849bde6f2e8828f29bdc92040bcb017396
NovaCrypt <novacrypt0512@pm.me>
2026-02-10 19:43:55 +0000

    Configure noctalia shell

--- noctalia.nix
+++ noctalia.nix
@@ -8,6 +8,65 @@
 
     programs.noctalia-shell = {
       enable = true;
+      settings = {
+        bar = {
+          position = "right";
+          density = "comfortable";
+          widgets = {
+            left = [
+              {
+                id = "Launcher";
+              }
+              {
+                id = "Clock";
+              }
+            ];
+            center = [
+              {
+                id = "Workspace";
+              }
+            ];
+            right = [
+              {
+                id = "Tray";
+              }
+              {
+                id = "NotificationHistory";
+              }
+              {
+                id = "Battery";
+              }
+              {
+                id = "Volume";
+              }
+              {
+                id = "Brightness";
+              }
+              {
+                id = "ControlCenter";
+              }
+            ];
+          };
+        };
+        general = {
+          dimmerOpacity = 0.15;
+          scaleRation = 0.9;
+          telemetryEnabled = true;
+        };
+        wallpaper = {
+          enabled = true;
+          directory = "/home/novus/Pictures/Wallpapers";
+        };
+        appLauncher = {
+          terminalCommand = "kitty";
+        };
+        dock = {
+          enabled = false;
+        };
+        colorSchemes = {
+          useWallpaperColors = true;
+        };
+      };
     };
   };
 }
