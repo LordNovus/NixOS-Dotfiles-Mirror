eeef98dd01aa70d9c3b99aafb9663f01f2720135
NovaCrypt <novacrypt0512@pm.me>
2026-02-11 15:33:00 +0000

    Customise noctalia settings

--- noctalia.nix
+++ noctalia.nix
@@ -50,21 +50,127 @@
         };
         general = {
           dimmerOpacity = 0.15;
-          scaleRation = 0.9;
+          scaleRatio = 0.85;
+          animationSpeed = 1.2;
+          shadowDirection = "bottom_left";
           telemetryEnabled = true;
+        };
+        ui = {
+          fontDefault = "Hack Nerd Font";
+          fontFixed = "DepartureMono Nerd Font";
+          boxBorderEnabled = true;
+        };
+        location = {
+          name = "Swindon, UK";
+          weatherShowEffects = false;
         };
         wallpaper = {
           enabled = true;
           directory = "/home/novus/Pictures/Wallpapers";
         };
         appLauncher = {
-          terminalCommand = "kitty";
+          terminalCommand = "kitty -e";
+          iconMode = "native";
+          overviewLayer = true;
+          enableSettingsSearch = false;
+        };
+        controlCenter = {
+          shortcuts = {
+            left = [
+              {
+                id = "Network";
+              }
+              {
+                id = "Bluetooth";
+              }
+              {
+                id = "PowerProfile";
+              }
+              {
+                id = "Notifications";
+              }
+            ];
+            right = [
+              {
+                id = "WallpaperSelector";
+              }
+              {
+                id = "NightLight";
+              }
+              {
+                id = "KeepAwake";
+              }
+            ];
+          };
+          cards = [
+            {
+              enabled = true;
+              id = "profile-card";
+            }
+            {
+              enabled = true;
+              id = "shortcuts-card";
+            }
+            {
+              enabled = false;
+              id = "audio-card";
+            }
+            {
+              enabled = false;
+              id = "brightness-card";
+            }
+            {
+              enabled = true;
+              id = "weather-card";
+            }
+            {
+              enabled = true;
+              id = "media-sysmon-card";
+            }
+          ];
+        };
+        systemMonitor = {
+          diskWarningThreshold = 60;
+          diskCriticalThreshold = 85;
+          diskAvailWarningThreshold = 40;
+          diskAvailCriticalThreshold = 15;
+          batteryWarningThreshold = 25;
+          batteryCriticalThreshold = 10;
         };
         dock = {
           enabled = false;
         };
+        notifications = {
+          monitors = [ "eDP-1" ];
+        };
         colorSchemes = {
           useWallpaperColors = true;
+          schedulingMode = "location";
+          generationMethod = "faithful";
+        };
+        templates = {
+          activeTemplates = [
+            {
+              enabled = true;
+              id = "niri";
+            }
+            {
+              enabled = true;
+              id = "gtk";
+            }
+            {
+              enabled = true;
+              id = "kitty";
+            }
+            {
+              enabled = true;
+              id = "cava";
+            }
+            {
+              enabled = true;
+              id = "qt";
+            }
+          ];
         };
       };
     };
