98061cdcefe8d986965e527a5033b108ce0e245c
NovaCrypt <novacrypt0512@pm.me>
2026-02-11 14:51:44 +0000

    Fix cursor theming

--- home.nix
+++ home.nix
@@ -73,14 +73,6 @@ in
     package = pkgs.vimix-icon-theme;
   };
 
-  # Cursor theme config
-  home.pointerCursor = {
-    enable = true;
-    name = "volantes-cursors";
-    package = pkgs.volantes-cursors;
-    size = 24;
-  };
-
   fonts.fontconfig.enable = true;
 
   home.stateVersion = "25.05";
