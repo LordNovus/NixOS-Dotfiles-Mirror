ae9df360160fe3b0d6a1819b53c003803c98e15b
NovaCrypt <novacrypt0512@pm.me>
2026-03-25 19:04:44 +0000

    Fix import for firefox

--- configuration.nix
+++ configuration.nix
@@ -52,7 +52,7 @@ in
       # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
       ./services.nix # System services
-      ./programs
+      ./programs/firefox.nix
     ];
 
   # Enable flakes 
